0a16d99f9c2aa13d62b2eb601048864332107d41
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-29 23:44:07 +0700

    Schema Validation với Yup

--- Shoppe_Clone/src/pages/Register/Register.tsx
+++ Shoppe_Clone/src/pages/Register/Register.tsx
@@ -1,13 +1,10 @@
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import Input from 'src/components/Input'
-import { getRules } from 'src/utils/rules'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Schema, schema } from 'src/utils/rules'
 
-interface FormData {
-  email: string
-  password: string
-  confirm_password: string
-}
+type FormData = Schema
 
 export default function Register() {
   const {
@@ -16,8 +13,10 @@ export default function Register() {
     watch,
     getValues,
     formState: { errors }
-  } = useForm<FormData>()
-  const rules = getRules(getValues)
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
+
   const onSubmit = handleSubmit(
     (data) => {
       // console.log(data)
@@ -45,7 +44,6 @@ export default function Register() {
                 className='mt-8'
                 errorMessage={errors.email?.message}
                 placeholder='Email'
-                rules={rules.email}
               />
               <Input
                 name='password'
@@ -54,7 +52,6 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.password?.message}
                 placeholder='Password'
-                rules={rules.password}
                 autoComplete='on'
               />
               <Input
@@ -64,7 +61,6 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.confirm_password?.message}
                 placeholder='Confirm Password'
-                rules={rules.confirm_password}
                 autoComplete='on'
               />
               <div className='mt-2'>
