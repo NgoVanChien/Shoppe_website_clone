d0a02eb885ec385b2685bc6ea54bfe2d1d14f1e9
Ngo Van Chien <chienngo03568@gmail.com>
2023-06-01 16:28:54 +0700

    Setup Protected Route và Rejected Route trong React Router

--- Shoppe_Clone/src/useRouteElements.tsx
+++ Shoppe_Clone/src/useRouteElements.tsx
@@ -1,34 +1,67 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import ResgisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
+import Profile from './pages/Profile'
+
+const isAuthenticated = true
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  // const isAuthenticated = false
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/',
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'register',
+          element: (
+            <ResgisterLayout>
+              <Register />
+            </ResgisterLayout>
+          )
+        },
+        {
+          path: 'login',
+          element: (
+            <ResgisterLayout>
+              <Login />
+            </ResgisterLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+
+    {
+      path: '',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
         </MainLayout>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <ResgisterLayout>
-          <Register />
-        </ResgisterLayout>
-      )
-    },
-    {
-      path: '/login',
-      element: (
-        <ResgisterLayout>
-          <Login />
-        </ResgisterLayout>
       )
     }
   ])
