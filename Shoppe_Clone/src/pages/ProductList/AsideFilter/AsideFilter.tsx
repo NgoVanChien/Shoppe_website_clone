10ea4c1c918959e3aa36ec143d43ae4cc88e2940
Ngo Van Chien <chienngo03568@gmail.com>
2023-06-02 15:54:45 +0700

    Code UI AsideFilter

---
+++ Shoppe_Clone/src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -0,0 +1,143 @@
+import { Link } from 'react-router-dom'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
+import path from 'src/constants/path'
+
+export default function AsideFilter() {
+  return (
+    <div className='py-4'>
+      <Link to={path.home} className='flex items-center font-bold'>
+        <svg viewBox='0 0 12 10' className='mr-3 h-4 w-3 fill-current'>
+          <g fillRule='evenodd' stroke='none' strokeWidth={1}>
+            <g transform='translate(-373 -208)'>
+              <g transform='translate(155 191)'>
+                <g transform='translate(218 17)'>
+                  <path d='m0 2h2v-2h-2zm4 0h7.1519633v-2h-7.1519633z' />
+                  <path d='m0 6h2v-2h-2zm4 0h7.1519633v-2h-7.1519633z' />
+                  <path d='m0 10h2v-2h-2zm4 0h7.1519633v-2h-7.1519633z' />
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+        Tất cả danh mục
+      </Link>
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <ul>
+        <li className='py-2 pl-2'>
+          <Link to={path.home} className='relative px-2 font-semibold text-orange'>
+            <svg viewBox='0 0 4 7' className='absolute left-[-10px] top-1 h-2 w-2 fill-orange'>
+              <polygon points='4 3.5 0 0 0 7' />
+            </svg>
+            Thời trang nam
+          </Link>
+        </li>
+        <li className='py-2 pl-2'>
+          <Link to={path.home} className='relative px-2 '>
+            Áo khoác
+          </Link>
+        </li>
+      </ul>
+      <Link to={path.home} className='mt-4 flex items-center font-bold uppercase'>
+        <svg
+          enableBackground='new 0 0 15 15'
+          viewBox='0 0 15 15'
+          x={0}
+          y={0}
+          className='mr-3 h-4 w-3 fill-current stroke-current'
+        >
+          <g>
+            <polyline
+              fill='none'
+              points='5.5 13.2 5.5 5.8 1.5 1.2 13.5 1.2 9.5 5.8 9.5 10.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeMiterlimit={10}
+            />
+          </g>
+        </svg>
+        Bộ lọc tìm kiếm
+      </Link>
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <div className='my-5'>
+        <div>Khoảng giá</div>
+        <form className='mt-2'>
+          <div className='flex items-start'>
+            <Input
+              type='text'
+              className='grow'
+              name='form'
+              placeholder='đ TỪ'
+              classNameInput='w-full rounded-sm border border-gray-300 p-1 outline-none focus:border-gray-500 focus:shadow-sm'
+            />
+            <div className='mx-2 mt-2 shrink-0'>-</div>
+            <Input
+              type='text'
+              className='grow'
+              name='form'
+              placeholder='đ ĐẾN'
+              classNameInput='w-full rounded-sm border border-gray-300 p-1 outline-none focus:border-gray-500 focus:shadow-sm'
+            />
+          </div>
+          <Button className='flex w-full items-center justify-center  bg-orange p-2 text-sm uppercase  text-white hover:bg-orange/80 '>
+            Áp dụng
+          </Button>
+        </form>
+      </div>
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <div className='text-sm'>Đánh giá</div>
+      <ul className='my-3'>
+        <li className='py-1 pl-2'>
+          <Link to='' className='flex items-center text-sm'>
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='#f1ff27'
+                  viewBox='0 0 24 24'
+                  strokeWidth='1.5'
+                  stroke='currentColor'
+                  className='h-6 w-6'
+                  key={index}
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
+                  />
+                </svg>
+              ))}
+            <span>trở lên</span>
+          </Link>
+          <Link to='' className='mr-1 flex items-center text-sm'>
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='#f1ff27'
+                  viewBox='0 0 24 24'
+                  strokeWidth='1.5'
+                  stroke='currentColor'
+                  className='h-6 w-6 '
+                  key={index}
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
+                  />
+                </svg>
+              ))}
+            <span>trở lên</span>
+          </Link>
+        </li>
+      </ul>
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <Button className='flex w-full items-center justify-center  bg-orange p-2 text-sm uppercase  text-white hover:bg-orange/80 '>
+        Xoá tất cả
+      </Button>
+    </div>
+  )
+}
