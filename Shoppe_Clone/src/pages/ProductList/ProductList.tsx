e817b5c96f5b7eb956d1be18ce7124e13dff6939
Ngo Van Chien <chienngo03568@gmail.com>
2023-06-03 15:29:35 +0700

    Code chức năng Pagination

--- Shoppe_Clone/src/pages/ProductList/ProductList.tsx
+++ Shoppe_Clone/src/pages/ProductList/ProductList.tsx
@@ -4,9 +4,12 @@ import Product from './Product'
 import SortProductList from './SortProductList'
 import productApi from 'src/apis/product.api'
 import useQueryParams from 'src/hooks/useQueryParams'
+import { useState } from 'react'
+import Pagination from 'src/components/Pagination'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
+  const [page, setPage] = useState(1)
   const { data } = useQuery({
     queryKey: ['products', queryParams],
     queryFn: () => {
@@ -31,6 +34,7 @@ export default function ProductList() {
                   </div>
                 ))}
             </div>
+            <Pagination page={page} setPage={setPage} pageSize={4} />
           </div>
         </div>
       </div>
