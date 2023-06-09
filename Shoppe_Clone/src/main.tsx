5132a081df9c030c96b7a38d4591aa088a71c0ba
Ngo Van Chien <chienngo03568@gmail.com>
2023-06-09 16:55:48 +0700

    Test App render và React Router

--- Shoppe_Clone/src/main.tsx
+++ Shoppe_Clone/src/main.tsx
@@ -1,40 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from 'src/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AppProvider } from './contexts/app.context.tsx'
-import ErrorBoundary from './components/ErrorBoundary.tsx'
 import 'src/i18n/i18n'
-import { HelmetProvider } from 'react-helmet-async'
-
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 0
-    }
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          {/* Provide the client to your App */}
-          <AppProvider>
-            <ErrorBoundary>
-              <App />
-            </ErrorBoundary>
-          </AppProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-        {/* The rest of your application */}
-      </HelmetProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
