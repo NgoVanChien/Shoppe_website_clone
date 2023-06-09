9b2ea38a4d812629b970e15051c67576acfa6ae1
Ngo Van Chien <chienngo03568@gmail.com>
2023-06-10 01:18:16 +0700

    Kỹ thuật test snapshot

--- Shoppe_Clone/src/msw/auth.msw.ts
+++ Shoppe_Clone/src/msw/auth.msw.ts
@@ -8,7 +8,7 @@ export const refresh_token_1000days =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xNVQxNDowNTozNS41MTVaIiwiaWF0IjoxNjcxMTEzMTM1LCJleHAiOjE3NTc1MTMxMzV9.OHDBqBjhih1fgNe6-mWo0PQ-IcukNz4ljlXUCxM-8V8'
 
 export const access_token =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODM2MTEwMWFmYzJlMWExZjk2OGZjZCIsImVtYWlsIjoiejJAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMy0wNi0wOVQxNzozMTo0Ny40NTJaIiwiaWF0IjoxNjg2MzMxOTA3LCJleHAiOjE2ODczMzE5MDZ9.3QjVMco7iCFnmApppMwk_1lv_fyA53SeyhOEo3x-6PU'
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODM2N2Q4MWFmYzJlMWExZjk2OGZkYyIsImVtYWlsIjoiejRAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMy0wNi0wOVQxODoxNjo0Mi4yMTZaIiwiaWF0IjoxNjg2MzM0NjAyLCJleHAiOjE2ODczMzQ2MDF9.mm7EQW2wxyDUlWDWSFWxFKgL19A2LvgICJZJzAgGhjU'
 
 const loginRes = {
   message: 'Đăng nhập thành công',
@@ -20,17 +20,15 @@ const loginRes = {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xOVQwNDoxODowMC4wNjRaIiwiaWF0IjoxNjcxNDIzNDgwLCJleHAiOjE3NTc4MjM0ODB9.AvavrdIeU1xm2KrFeEKSiDJs260YU1uWxRzVw30MgoU',
     expires_refresh_token: 86400000,
     user: {
-      _id: '648361101afc2e1a1f968fcd',
+      _id: '648367d81afc2e1a1f968fdc',
       roles: ['User'],
-      email: 'z2@gmail.com',
-      createdAt: '2023-06-09T17:27:44.279Z',
-      updatedAt: '2023-06-09T17:30:53.871Z',
+      email: 'z4@gmail.com',
+      createdAt: '2023-06-09T17:56:40.726Z',
+      updatedAt: '2023-06-09T17:57:26.365Z',
       __v: 0,
-      address: 'VN',
-      avatar: 'f16d4549-6ed1-45bf-bb23-81a06e9a6697.jpg',
-      date_of_birth: '2002-02-19T17:00:00.000Z',
-      name: 'Otis',
-      phone: '0123456789'
+      avatar: '104c4f48-137c-46d3-80bc-08e2b7532d2f.jpg',
+      date_of_birth: '1989-12-31T17:00:00.000Z',
+      name: 'Mix'
     }
   }
 }
